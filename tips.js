358289b05b834bf51da47c9a4b21eb976ca01f1f
neelimapriya <neelimasultana6@gmail.com>
2023-08-31 10:51:52 +0600

    second commit

---
+++ tips.js
@@ -0,0 +1,16 @@
+
+const person = {
+    name: "mehedy",
+    age: 26
+}
+
+// const {name:myName} = person;
+// console.log(myName)
+
+function show ({name,age}){
+
+    // const {name,age} = obj;
+
+    console.log(`my name is ${name}. My age is ${age}`)
+}
+
